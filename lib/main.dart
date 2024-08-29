29f8e4ffe83a5b02e50b343365edf76458ee949d
Igiz <igizstarkle@gmail.com>
2024-08-29 10:02:47 +0300

    const modifier

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-     MaterialApp(
+    MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
-              colors: [
-                Colors.deepOrange,
-                Colors.orangeAccent
-              ],
+              colors: [Colors.deepOrange, Colors.orangeAccent],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topRight,
             ),
           ),
-          child: Center(
-            child: Text('Hello World'),
+          child: const Center(
+            child: Text(
+              'Hello World',
+              style: TextStyle(
+                fontSize: 28,
+                backgroundColor: Colors.white,
+              ),
+            ),
           ),
         ),
       ),
